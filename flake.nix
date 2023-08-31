67c91a51fa7a94546cc4af13fcdfa3828d3e0d8d
pavanbhat1999 <prbhat07@gmail.com>
2023-08-31 21:55:51 +0530

    update

--- flake.nix
+++ flake.nix
@@ -20,7 +20,6 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-
             home-manager.users.root99 = import ./home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
