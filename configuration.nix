ddd6603d97fbe658a568e587f467d61bfac5a26c
pavanbhat1999 <prbhat07@gmail.com>
2023-09-03 09:19:39 +0530

    nvidia support added

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,8 @@
   };
   hardware.opengl = {
     enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
@@ -33,7 +35,38 @@
       libvdpau-va-gl
     ];
   };
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
 
+    # Modesetting is needed most of the time
+    modesetting.enable = true;
+
+	# Enable power management (do not disable this unless you have a reason to).
+	# Likely to cause problems on laptops and with screen tearing if disabled.
+	powerManagement.enable = true;
+
+    # Use the NVidia open source kernel module (which isn't “nouveau”).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+	hardware.nvidia.prime = {
+        offload = {
+            enable = true;
+            enableOffloadCmd = true;
+        };
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+	};
   programs.dconf.enable = true;
   virtualisation.libvirtd.enable = true;
   # Set your time zone.
@@ -237,7 +270,6 @@
   };
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
-  # services.xserver.videoDrivers = [ "nvidia" ];
   # hardware.opengl.enable = true;
 
   # Enable the OpenSSH daemon.
